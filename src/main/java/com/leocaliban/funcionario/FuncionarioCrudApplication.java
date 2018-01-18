8f137f502a722da1d572b15371d195d1738fdb27
Leocaliban <leocaliban@gmail.com>
2018-01-18 03:18:02 -0300

    Cria projeto

---
+++ src/main/java/com/leocaliban/funcionario/FuncionarioCrudApplication.java
@@ -0,0 +1,12 @@
+package com.leocaliban.funcionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuncionarioCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuncionarioCrudApplication.class, args);
+	}
+}
