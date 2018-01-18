ffcf3769a1eb48b0ace9b7bd87e571bd42e47d05
Leocaliban <leocaliban@gmail.com>
2018-01-18 03:51:18 -0300

    Faz o mapeamento JPA e conexão com o banco H2

--- src/main/java/com/leocaliban/funcionario/domain/Funcionario.java
+++ src/main/java/com/leocaliban/funcionario/domain/Funcionario.java
@@ -3,10 +3,18 @@ package com.leocaliban.funcionario.domain;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Funcionario implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	
 	private String nome;
